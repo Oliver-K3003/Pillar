959a0919f5505831dba259549355a0e949e8077f
Oliver-K3003 <60487795+Oliver-K3003@users.noreply.github.com>
2024-11-16 17:48:03 -0500

    Added basic mistral integration
    Integrated basic mistral nemo model into backend
    Added some output formatting
    Added some automatic output styling for provided responses

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,21 @@
-from flask import Flask
+import json
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from mistralTest import sendReq, parseOutput
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-@app.route('/get-resp', methods=['GET'])
+@app.route('/get-resp', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def getResp():
+    data = request.json
+    prompt = data.get('prompt', 'No Prompt Given')
     # will be filled in with function to gather resp
-    return 'test resp'
+    promptResponse = json.loads(sendReq(prompt))
+    promptResponse = parseOutput(promptResponse)
+    return promptResponse
 
 
 if __name__ == "__main__":
---
+++ backend/mistralTest.py
@@ -0,0 +1,46 @@
+import os
+import json
+from mistralai import Mistral
+
+def sendReq(userContent : str) -> str:
+
+    apiKey = os.environ.get('MISTRAL_API_KEY', 'BROKEN')
+    model="open-mistral-nemo"
+
+    client = Mistral(api_key=apiKey)
+
+    chatResponse = client.chat.complete(
+        model=model,
+        messages= [
+            {
+                "role": "system",
+                "content": "You are a GitHub issue assistant, you must always answer with suggestions to solve given problems. Return the message split into a body, examples, and supporting information as a short JSON object. Examples and supporting information should be arrays and all text and links should be formatted in markdown format"
+            },
+            {
+                "role": "user",
+                "content": userContent,
+            },
+        ]
+    )
+
+    return chatResponse.choices[0].message.content
+
+def parseOutput(output: dict) -> str:
+    outputStr = f'{output.get('body')}\n'
+
+    for ex in output.get('examples'):
+        outputStr = outputStr+f'{ex}\n'
+
+    for si in output.get('supporting_information'):
+        outputStr = outputStr+f'{si}\n'
+
+    return outputStr
+
+# print(chatResponse.choices[0].message.content)
+
+if __name__ == '__main__':
+    msg = 'What is functional programming?'
+
+    resp = sendReq(msg)
+
+    print(parseOutput(json.loads(resp)))
