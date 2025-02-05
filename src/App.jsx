bd8c71d44f58d28c23b3e285c3e6e11a427cdeb1
Oliver-K3003 <Oliverkramer3003@gmail.com>
2025-02-04 21:50:29 -0500

    added typing effect to text display

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import "./App.css";
 // general imports
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ReactMarkdown from "react-markdown";
-// plugin imports
-import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
 // asset imports
 import submitArrow from "./assets/submit_arrow.svg";
 import fullLogo from "./assets/pillar_logo_full.svg";
@@ -26,140 +21,128 @@ TODO
     under user-profile, resp-profile, & resp-msg)
 */
 
-function App() {
-  const [userMsgs, setUserMsgs] = useState([]);
-  const [respMsgs, setRespMsgs] = useState([]);
-  const [combinedMsgs, setCombinedMsgs] = useState([]);
-  const [msgVal, setMsgVal] = useState("");
+function ResponseMessage({ msg, timeout = 1000 }) {
+    const [contents, setContents] = useState("");
+    const [finishedMsg, setFinishedMsg] = useState(false);
+    let i = 0;
 
-  useEffect(() => dispMsgs(), [userMsgs]);
-
-  useEffect(() => dispMsgs(), [respMsgs]);
-
-  const handleInput = (e) => {
-    setMsgVal(e.target.value);
-  };
-
-  const getResp = (e) => {
-    e.preventDefault();
-
-    e.target.querySelector('input').value = "";
-
-    // create deep copy
-    let newUserMsgs = JSON.parse(JSON.stringify(userMsgs));
-
-    newUserMsgs.push(msgVal);
-
-    setUserMsgs(newUserMsgs);
-    console.log(msgVal);
-    // sessionStorage.setItem("user-msgs", JSON.stringify(newUserMsgs));
-    // get response from API server
-    axios
-      .post("http://localhost:5000/get-resp", { prompt: msgVal })
-      .then((resp) => {
-        console.log(resp.data);
-        // deep copy of resp msg list
-        let newRespMsgs = JSON.parse(JSON.stringify(respMsgs));
-
-        // add new data to list
-        newRespMsgs.push(resp.data);
-
-        // update state with new msgs
-        setRespMsgs(newRespMsgs);
-        // update session storage with new msgs
-        // sessionStorage.setItem("resp-msgs", JSON.stringify(newRespMsgs));
-      })
-      .catch((err) => console.error(`Error in getResp: ${err}`));
-  };
-
-  const dispMsgs = () => {
-    // ensure msgs exist
-    if (userMsgs !== null) {
-      if (respMsgs !== null) {
-        // if user and resp msgs exist alternate them
-        let msgChain = [];
-        let l = Math.min(userMsgs.length, respMsgs.length);
-
-        for (let i = 0; i < l; i++) {
-          msgChain.push(userMsgs[i], respMsgs[i]);
-        }
-        msgChain.push(...userMsgs.slice(l), ...respMsgs.slice(l));
-
-        setCombinedMsgs(msgChain);
-      } else {
-        setCombinedMsgs(userMsgs);
-      }
-    } else {
-      return <></>;
+    if (contents === "" && !finishedMsg) {
+        const interval = setInterval(() => {
+            if (i < msg.res.length) {
+                i++;
+                setContents(msg.res.slice(0, i));
+            } else {
+                clearInterval(interval);
+                setFinishedMsg(true);
+            }
+        }, timeout);
     }
-  };
 
-  return (
-    <div className="container">
-      <div className="background">
-        {userMsgs.length < 1 ? <img src={fullLogo} alt="" /> : <></>}
-      </div>
-      <div className="message-list">
-        {
-          <>
-            {combinedMsgs.map((msg, i) => (
-              <>
-                <div
-                  key={i+1}
-                  // replace user-profile with common 'hidden' class if we decide to go forth with that display method
-                  className={`${
-                    i % 2 === 0 ? "user-profile" : "resp-profile"
-                  } profile`}
+    return (
+        <>
+            <div
+                className="resp-profile profile"
+            >
+                <img key={i} src={blackLogo} alt="" />
+            </div>
+            <div
+                className="resp-msg msg"
+            >
+                {contents}
+                {!finishedMsg && 
+                        <svg
+                    viewBox="8 0 8 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="cursor"
+                        >
+                        <rect x="10" y="2" width="6" height="14" fill="#fff" />
+                        </svg>
+                }
+            </div>
+        </>
+    )
+}
+
+function UserMessage({ msg }) {
+    return (
+        <>
+            <div
+                className="user-profile profile"
+            >
+            </div>
+            <div
+                className="user-msg msg"
+            >
+                {msg.usr}
+            </div>
+        </>
+    )
+}
+
+function App() {
+    const [msgs, setMsgs] = useState([]);
+    const [msgVal, setMsgVal] = useState("");
+
+    const handleInput = (e) => {
+        setMsgVal(e.target.value);
+    };
+
+    const getResp = (e) => {
+        e.preventDefault();
+
+        e.target.querySelector("input").value = "";
+
+        // create deep copy
+        let newMsgs = [...msgs, { "usr": msgVal }];
+
+        console.log(newMsgs);
+        setMsgs(newMsgs);
+        // get response from API server
+        axios
+            .post("http://localhost:5000/get-resp", { prompt: msgVal })
+            .then((resp) => {
+                console.log(resp.data);
+                // deep copy of resp msg list
+                // add new data to list
+                newMsgs = [...newMsgs, { "res": resp.data }];
+                console.log(newMsgs);
+
+                // update state with new msgs
+                setMsgs(newMsgs);
+            })
+            .catch((err) => console.error(`Error in getResp: ${err}`));
+    };
+
+    return (
+        <div className="container">
+            <div className="background">
+                {msgs.length < 1 ? <img src={fullLogo} alt="" /> : <></>}
+            </div>
+            <div className="message-list">
+                {msgs.map((msg, i) => {
+                    console.log(msg);
+                    if (msg !== undefined) {
+                        return (
+                            Object.keys(msg).includes("usr") ? <UserMessage msg={msg} key={i} /> : <ResponseMessage msg={msg} timeout={50} key={i} />
+                        )
+                    }
+                })}
+            </div>
+
+            <form className="chat-bar" onSubmit={(e) => getResp(e)}>
+                <input
+                    type="text"
+                    placeholder="Message Pillar"
+                    onChange={handleInput}
+                />
+                <button
+                    type="button"
                 >
-                  <img key={i} src={blackLogo} alt="" />
-                </div>
-                <div
-                  key={i}
-                  className={`${i % 2 === 0 ? "user-msg" : "resp-msg"} msg`}
-                >
-                  <ReactMarkdown
-                    remarkPlugins={[remarkGfm]}
-                    components={{
-                      code({ node, inline, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-                        return !inline && match ? (
-                          <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, "")}
-                            style={vs}
-                            language={match[1]}
-                            PreTag="div"
-                            {...props}
-                          />
-                        ) : (
-                          <code className={className} {...props}>
-                            {children}
-                          </code>
-                        );
-                      },
-                    }}
-                  >
-                    {msg}
-                  </ReactMarkdown>
-                </div>
-              </>
-            ))}
-          </>
-        }
-      </div>
-      <form className="chat-bar" onSubmit={(e) => getResp(e)}>
-          <input
-          type="text"
-          placeholder="Message Pillar"
-          onChange={handleInput}
-          />
-          <button
-          type="button"
-          >
-          <img src={submitArrow} alt="" />
-          </button>
-      </form>
-    </div>
-  );
+                    <img src={submitArrow} alt="" />
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
