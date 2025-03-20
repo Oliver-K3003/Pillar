18ea80a1c5710f6001f51630d84c4998bef0ff4a
Oliver-K3003 <Oliverkramer3003@gmail.com>
2025-03-19 21:13:00 -0400

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,6 @@ import collapseIcon from "./assets/collapse-icon.svg";
 import expandIcon from "./assets/expand-icon.svg";
 import GithubLogin from "./components/GithubLogin";
 
-/* 
-TODO
-  [ ] iron out profile pictures (not sure if we want user to have one)
-  [ ] clean up color scheme
-  [ ] optimize for extension format
-  [ ] fix retyping on reload/change chat
-*/
-
 function App() {
     const [isSideNavCollapsed, setSideNavCollapsed] = useState(false);
     const [chatIds, setChatIds] = useState([]);
@@ -87,52 +79,116 @@ function App() {
             });
     }
 
+    const Login = () => (
+        <div className="container">
+            Please Login To use Pillar
+            <GithubLogin setUser={setUser} />
+            <div className="background">
+                <img src={fullLogo} alt="" />
+            </div>
+        </div>
+    );
+
     return (
         <Router>
-            <div className="container">
-                <GithubLogin setUser={setUser}/>
-                <div className="sideNav">
-                    <Sidebar collapsed={isSideNavCollapsed}>
-                        <div className="sideNavHeader">
-                            <button className="headerButton" type="button" onClick={() => setSideNavCollapsed(!isSideNavCollapsed)}>
-                                {!isSideNavCollapsed ? <img className="headerButtonIcon" src={collapseIcon} alt="" /> : <img className="headerButtonIcon" src={expandIcon} alt="" />}
-                            </button>
-                            {!isSideNavCollapsed ? <button className="headerButton" type="button" onClick={createNewChat}>
-                                <img className="headerButtonIcon" src={newChat} alt="" />
-                            </button> : null}
-                        </div>
-                        <Menu className="chat-container" title="Pillar" menuItemStyles={{
-                            button: ({ level, active, disabled }) => {
-                                if (level === 0 || level === 1) {
-                                    return {
-                                        transition: "backgroundColor 200ms ease-in-out",
-                                        zIndex: "100",
-                                        "&:hover": {
-                                            backgroundColor: "#403f3f !important",
-                                            zIndex: "100"
-                                        }
+            <Routes>
+
+                <Route path="/" element={
+                    <>
+                        <div className={`container ${user ? "" : "hidden"}`}>
+                            <div className="sideNav">
+                                <Sidebar collapsed={isSideNavCollapsed}>
+                                    <div className="sideNavHeader">
+                                        <button className="headerButton" type="button" onClick={() => setSideNavCollapsed(!isSideNavCollapsed)}>
+                                            {!isSideNavCollapsed ? <img className="headerButtonIcon" src={collapseIcon} alt="" /> : <img className="headerButtonIcon" src={expandIcon} alt="" />}
+                                        </button>
+                                        {!isSideNavCollapsed ? <button className="headerButton" type="button" onClick={createNewChat}>
+                                            <img className="headerButtonIcon" src={newChat} alt="" />
+                                        </button> : null}
+                                    </div>
+                                    <Menu className="chat-container" title="Pillar" menuItemStyles={{
+                                        button: ({ level, active, disabled }) => {
+                                            if (level === 0 || level === 1) {
+                                                return {
+                                                    transition: "backgroundColor 200ms ease-in-out",
+                                                    zIndex: "100",
+                                                    "&:hover": {
+                                                        backgroundColor: "#403f3f !important",
+                                                        zIndex: "100"
+                                                    }
+                                                }
+                                            }
+                                        },
                                     }
+                                    }>
+                                        <SubMenu label="Chats">
+                                            {chatIds.map((id) =>
+                                                <MenuItem key={id} id={id}>
+                                                    <div className="menuItemContainer">
+                                                        <Link className="chat-link" to={`/chat/${id}`}>Chat {id}</Link>
+                                                        <button className="headerButton" onClick={() => deleteChat(id)}><img className="headerButtonIcon" src={deleteIcon} /></button>
+                                                    </div>
+                                                </MenuItem>
+                                            )}
+                                        </SubMenu>
+                                    </Menu>
+                                </Sidebar>
+                            </div>
+                            <Home />
+                        </div>
+                        <div className={`container ${user ? "hidden" : ""}`}>
+                            <div className="login">
+                                <img src={fullLogo} alt="" />
+                                <GithubLogin setUser={setUser} />
+                            </div>
+                        </div>
+                    </>
+                } />
+
+                <Route path="/chat/:chatId" element={user ? <>
+                    <div className="container">
+                        <div className="sideNav">
+                            <Sidebar collapsed={isSideNavCollapsed}>
+                                <div className="sideNavHeader">
+                                    <button className="headerButton" type="button" onClick={() => setSideNavCollapsed(!isSideNavCollapsed)}>
+                                        {!isSideNavCollapsed ? <img className="headerButtonIcon" src={collapseIcon} alt="" /> : <img className="headerButtonIcon" src={expandIcon} alt="" />}
+                                    </button>
+                                    {!isSideNavCollapsed ? <button className="headerButton" type="button" onClick={createNewChat}>
+                                        <img className="headerButtonIcon" src={newChat} alt="" />
+                                    </button> : null}
+                                </div>
+                                <Menu className="chat-container" title="Pillar" menuItemStyles={{
+                                    button: ({ level, active, disabled }) => {
+                                        if (level === 0 || level === 1) {
+                                            return {
+                                                transition: "backgroundColor 200ms ease-in-out",
+                                                zIndex: "100",
+                                                "&:hover": {
+                                                    backgroundColor: "#403f3f !important",
+                                                    zIndex: "100"
+                                                }
+                                            }
+                                        }
+                                    },
                                 }
-                            },
-                        }
-                        }>
-                            <SubMenu label="Chats">
-                                {chatIds.map((id) =>
-                                    <MenuItem key={id} id={id}>
-                                        <div className="menuItemContainer">
-                                            <Link className="chat-link" to={`/chat/${id}`}>Chat {id}</Link>
-                                            <button className="headerButton" onClick={() => deleteChat(id)}><img className="headerButtonIcon" src={deleteIcon} /></button>
-                                        </div>
-                                    </MenuItem>
-                                )}
-                            </SubMenu>
-                        </Menu>
-                    </Sidebar>
-                </div>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/chat/:chatId" element={user ? <Chat /> : <Home />} />
-                </Routes></div>
+                                }>
+                                    <SubMenu label="Chats">
+                                        {chatIds.map((id) =>
+                                            <MenuItem key={id} id={id}>
+                                                <div className="menuItemContainer">
+                                                    <Link className="chat-link" to={`/chat/${id}`}>Chat {id}</Link>
+                                                    <button className="headerButton" onClick={() => deleteChat(id)}><img className="headerButtonIcon" src={deleteIcon} /></button>
+                                                </div>
+                                            </MenuItem>
+                                        )}
+                                    </SubMenu>
+                                </Menu>
+                            </Sidebar>
+                        </div>
+                        <Chat /></div>
+                </> : <Home />} />
+
+            </Routes>
         </Router>
     );
 }
--- src/components/GithubLogin.jsx
+++ src/components/GithubLogin.jsx
@@ -1,55 +1,50 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const GithubLogin = ({ setUser }) => {
+    const navigate = useNavigate();
 
-  function githubLoginRedirect() {
-    console.log("Clicked GitHub login button, sending GitHub login request.");
+    function githubLoginRedirect() {
+        console.log("Clicked GitHub login button, sending GitHub login request.");
 
-    axios.get(`/api/login/github`)
-      .then((response) => {
-        if (response.data.status === "Successful") {
-          console.log(response.data.github_auth_code_url);
-          const popup = window.open(response.data.github_auth_code_url, "_blank", "width=500,height=800"); // Open login in popup.
+        axios.get(`/api/login/github`)
+            .then((response) => {
+                if (response.data.status === "Successful") {
+                    console.log(response.data.github_auth_code_url);
+                    const popup = window.open(response.data.github_auth_code_url, "_blank", "width=500,height=800"); // Open login in popup.
 
-          let codeReceived = false;
+                    const timer = setInterval(() => {
+                        if (popup.closed) {
+                            clearInterval(timer);
 
-          // Check to see if the code was successfully received by the backend.
-          window.addEventListener("message", async (event) => {
-            if (event.origin !== "http://localhost:5000") return; // Ensure the event is from our backend
+                            // Fetch user info on on successful login.
+                            axios.get(`/api/github/user-info`)
+                                .then((response) => {
+                                    console.log(response.data.login);
+                                    setUser(response.data.login);
+                                })
+                                .catch((error) => {
+                                    console.log("Error getting user info. FETCH");
+                                });
+                        }
+                    }, 500);
 
-            const { code } = event.data;
-            if (code) {
-              codeReceived = true;
-              console.log("Received OAuth Code:", code);
-              popup.close(); // Auto-close the popup
-            }
-          }, false);
+                } else {
+                    console.log("Error within backend function /login/github")
+                }
+            })
+            .catch((error) => {
+                console.error("Axios error with request to /login/github");
+            });
+    }
 
-          // Fetch user info on on successful login.
-          axios.get(`/api/github/user-info`)
-          .then((response) => {
-              console.log(response.data.login);
-              setUser(response.data.login);
-          })
-          .catch((error) => {
-              console.log("Error getting user info.");
-          });
-
-        } else {
-          console.log("Error within backend function /login/github")
-        }
-      })
-      .catch((error) => {
-        console.error("Axios error with request to /login/github");
-      });
-  }
-
-  return (
-    <>
-      {/* Send request to backend to check if we're already logged in (ie. have token in this session.) */}
-      <button onClick={githubLoginRedirect} className="github-login">Login Using GitHub</button>
-    </>
-  )
+    return (
+        <div className="github-content">
+            <p>Please login to begin using Pillar!</p>
+            {/* Send request to backend to check if we're already logged in (ie. have token in this session.) */}
+            <button onClick={githubLoginRedirect} className="github-login">Login Using GitHub</button>
+        </div>
+    )
 }
 
 export default GithubLogin;
